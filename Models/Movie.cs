0a8d3ad8cdb23031a08d4eb865e6679817e498cd
s18310 <s18310@pjwstk.edu.pl>
2020-05-31 18:36:18 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
